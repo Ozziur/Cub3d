d511b045fd2528263b1177af23e58947e63b4cb3
Alice Novelli <anovelli@e3r4p3.42roma.it>
2023-01-24 15:36:48 +0100

    cub3d

---
+++ srcs/game/moves.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   moves.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anovelli <anovelli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 15:22:50 by anovelli          #+#    #+#             */
+/*   Updated: 2023/01/24 15:35:55 by anovelli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incl/cub3d.h"
+/*calcolati i valori del coseno e del seno dell'angolo di visuale del giocatore moltiplicati per la velocità del giocatore.*/
+void	moves(t_rules *rules, char d)
+{
+	float	rcos;
+	float	rsin;
+
+	
+}
