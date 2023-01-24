4a73711e48775f658276e0e6997609849b412de2
Alice Novelli <anovelli@e3r4p3.42roma.it>
2023-01-24 13:38:45 +0100

    cub3d

--- incl/cub3d.h
+++ incl/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: anovelli <anovelli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 11:23:28 by anovelli          #+#    #+#             */
-/*   Updated: 2023/01/24 13:36:52 by anovelli         ###   ########.fr       */
+/*   Updated: 2023/01/24 13:38:44 by anovelli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -134,7 +134,7 @@ void			game(t_rules *rules);
 //			raycast.c
 void			raycast(t_rules *rules, t_image *view, t_image *minimap);
 //			raycastcalc
-void	raycast_calc(t_bres_data *data, t_rules *rules,
+void			raycast_calc(t_bres_data *data, t_rules *rules,
 			t_image *view, t_image *minimap);
 //			debug
 void			debug(char *s);
