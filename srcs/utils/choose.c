7259ca2fe8737f0f3e598cd24a03024d716df1b5
MRuizzo <casaruizzo@yahoo.it>
2023-01-31 17:29:08 +0100

    cub3d

--- srcs/utils/choose.c
+++ srcs/utils/choose.c
@@ -6,7 +6,7 @@
 /*   By: mruizzo <mruizzo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 14:26:13 by mruizzo           #+#    #+#             */
-/*   Updated: 2023/01/24 18:23:43 by mruizzo          ###   ########.fr       */
+/*   Updated: 2023/01/31 17:29:03 by mruizzo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,17 @@ t_image	*choose_texture(t_rules *rules, t_bres_data *d)
 		&& (d->ray_angle < M_PI_2 || d->ray_angle > 3 * M_PI_2))
 		return (rules->inpmap.east_wall);
 	else if (!our_modulo(d->xy2[0], rules->inpmap.block_width)
-		&& (d->ray_angle >= M_PI_2 || d->ray_angle <= 3 * M_PI_2))
+		&& (d->ray_angle >= M_PI_2 && d->ray_angle <= 3 * M_PI_2))
 		return (rules->inpmap.west_wall);
 	else if (!our_modulo(d->xy2[1], rules->inpmap.block_width)
-		&& (d->ray_angle <= M_PI || d->ray_angle >= 0))
+		&& (d->ray_angle <= M_PI && d->ray_angle >= 0))
 		return (rules->inpmap.south_wall);
 	else if (!our_modulo(d->xy2[1], rules->inpmap.block_width)
-		&& (d->ray_angle > M_PI || d->ray_angle <= 2 * M_PI))
+		&& (d->ray_angle > M_PI && d->ray_angle <= 2 * M_PI))
 		return (rules->inpmap.north_wall);
 	else
 		ft_exit("fra non lo so facci passare pls");
 	return (NULL);
-}
 
 int	choose_x(t_draw_info *info, float ref, t_rules *rules)
 {
