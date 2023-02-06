ed509623474e834fa782b7fbc1ce1f8058d4e07e
MRuizzo <casaruizzo@yahoo.it>
2023-02-06 15:50:33 +0100

    cub3d

--- bonus/utils/sprite.c
+++ bonus/utils/sprite.c
@@ -6,7 +6,7 @@
 /*   By: mruizzo <mruizzo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 15:34:31 by mruizzo           #+#    #+#             */
-/*   Updated: 2023/02/06 15:42:49 by mruizzo          ###   ########.fr       */
+/*   Updated: 2023/02/06 15:50:12 by mruizzo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,19 @@
 
 static void load_sprite(t_image *coll, char *path)
 {
-	
+	coll = malloc(sizeof(t_image));
+	if (!coll)
+		ft_exit("malloc error");
+	coll->img = mlx_xpm_file_to_image(rules->mlx.mlx,
+			path, &coll->width, &coll->height);
+	if (!coll->img)
+		ft_exit("Error: can't open xpm file");
+	coll->addr = mlx_get_data_addr(coll->img, &coll->bpp,
+			&coll->line_length, &coll->endian);
 }
 
 void init_sprite(t_rules *rules)
 {
-	load_sprite(rules->skull[0], "");
+	load_sprite(rules->skull[0], "textures/64/coll/skull-0.xpm");
+	load_sprite(rules->skull[1], "textures/64/coll/skull-1.xpm");
 }
