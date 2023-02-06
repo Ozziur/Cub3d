a6f2fa183c0080d9c1bf8279fd5b548de6539650
MRuizzo <casaruizzo@yahoo.it>
2023-02-06 15:51:34 +0100

    cub3d

--- bonus/utils/sprite.c
+++ bonus/utils/sprite.c
@@ -6,13 +6,13 @@
 /*   By: mruizzo <mruizzo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 15:34:31 by mruizzo           #+#    #+#             */
-/*   Updated: 2023/02/06 15:50:12 by mruizzo          ###   ########.fr       */
+/*   Updated: 2023/02/06 15:51:09 by mruizzo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/cub3d_bonus.h"
 
-static void load_sprite(t_image *coll, char *path)
+static void load_sprite(t_image *coll, char *path, t_rules *rules)
 {
 	coll = malloc(sizeof(t_image));
 	if (!coll)
@@ -27,6 +27,6 @@ static void load_sprite(t_image *coll, char *path)
 
 void init_sprite(t_rules *rules)
 {
-	load_sprite(rules->skull[0], "textures/64/coll/skull-0.xpm");
-	load_sprite(rules->skull[1], "textures/64/coll/skull-1.xpm");
+	load_sprite(rules->skull[0], "textures/64/coll/skull-0.xpm", rules);
+	load_sprite(rules->skull[1], "textures/64/coll/skull-1.xpm", rules);
 }
