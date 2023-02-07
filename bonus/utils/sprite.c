59ab19eec6b544443ec3e8e95b8fc934bb899fae
MRuizzo <casaruizzo@yahoo.it>
2023-02-07 14:52:30 +0100

    init_sprite?

--- bonus/utils/sprite.c
+++ bonus/utils/sprite.c
@@ -3,14 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   sprite.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anovelli <anovelli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mruizzo <mruizzo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 15:34:31 by mruizzo           #+#    #+#             */
-/*   Updated: 2023/02/07 14:12:02 by anovelli         ###   ########.fr       */
+/*   Updated: 2023/02/07 14:46:36 by mruizzo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/cub3d_bonus.h"
+
+void	init_xpm_img(t_rules *rules, t_image **img, char *path)
+{
+	(*img) = malloc(sizeof(t_image));
+	if (!(*img))
+		die("Malloc error");
+	(*img)->img = mlx_xpm_file_to_image(rules->mlx.mlx, path,
+			&(*img)->width, &(*img)->height);
+	if (!(*img)->img)
+		die("Error initializing image");
+	(*img)->addr = mlx_get_data_addr((*img)->img, &(*img)->bpp,
+			&(*img)->line_length, &(*img)->endian);
+}
 
 static void	clear_sprites(t_rules *rules, t_sprite *spr)
 {
